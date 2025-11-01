31200de50874f2e39cf28e87babdd5abc0846081
Alexis Rinaldoni <alexis.rinaldoni@enviam.de>
2025-11-01 06:02:12 +0100

    Do not return result beautified

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,24 @@ import { stdin } from "process";
 import { parse } from "./lib/parse";
 
 const parseFromStdin = async () => {
-  let content = "";
-  stdin.setEncoding("utf-8");
-  stdin.on("data", (chunk) => {
-    content += chunk;
-  });
-  stdin.on("error", (err) => {
-    console.error("Error reading from stdin:", err);
-    process.exit(1);
-  });
-  stdin.on("end", () => {
-    const parsed = parse(content);
-    console.log(JSON.stringify(parsed, null, 2));
-    process.exit(0);
+  return new Promise<Record<string, any>>((resolve, reject) => {
+    let content = "";
+    stdin.setEncoding("utf-8");
+    stdin.on("data", (chunk) => {
+      content += chunk;
+    });
+    stdin.on("error", (err) => {
+      const error = new Error(`Error reading from stdin: ${err.message}`);
+      error.cause = err;
+      reject(error);
+    });
+    stdin.on("end", () => {
+      const parsed = parse(content);
+      resolve(parsed);
+    });
   });
 };
 
-await parseFromStdin();
+const result = await parseFromStdin();
+const resultString = JSON.stringify(result);
+console.log(resultString);
